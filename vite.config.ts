424563d9796cfdc49d18ff2778df8914623aecc5
Marcos Bião <dev.marcosdly@gmail.com>
2026-07-21 23:01:20 -0300

    fix: lib_plurals_parser variable removed during bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,29 @@
-import { defineConfig, type UserConfig } from "vite";
+import { defineConfig, type PluginOption, type UserConfig } from "vite";
+
+function pluginPreserveGeneratedModule(name: string): PluginOption {
+  const END_STATEMENT = ";";
+  return {
+    name: "preserve-generated-module",
+    enforce: "pre",
+    transform(rawCodeString, filePath) {
+      if (filePath.endsWith(`${name}.js`)) {
+        return {
+          code: rawCodeString.concat(
+            END_STATEMENT,
+            `window.${name} = ${name}`,
+            END_STATEMENT,
+          ),
+        };
+      }
+    },
+  } satisfies PluginOption;
+}
 
 export default defineConfig(({ mode }) => {
   const isDev = mode === "development";
   const isProd = mode === "production";
   return {
+    plugins: [pluginPreserveGeneratedModule("lib_plurals_parser")],
     build: {
       lib: {
         name: "jed",
